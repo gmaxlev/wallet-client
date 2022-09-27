1e7e6863212953938d00a7dfceab2c0da6353034
Max Levytskyi <immaxlev@gmail.com>
2022-09-27 16:14:52 +0300

    feat: validation-rules

---
+++ src/common/validation-rules.ts
@@ -0,0 +1,10 @@
+import * as Yup from "yup";
+
+export const emailRule = Yup.string()
+  .email("fields.email.errors.email")
+  .required("fields.email.errors.required")
+  .trim();
+
+export const passwordRule = Yup.string()
+  .required("fields.password.errors.required")
+  .trim();
